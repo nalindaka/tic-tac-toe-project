2d4d4fe72eb292d551cfc702f60a60c3021df712
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-11-06 11:25:12 +1000

    Conditional rendering of text in edit button and fix state change

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing((editing) => !editing);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="text" required value={name} />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
