f708bcff00739588e9da107ca259a4619a4a67a1
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-11-06 14:25:57 +1000

    Add name editing capability

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleClick() {
     setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
   return (
     <li>
       <span className="player">
         {!isEditing ? (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         ) : (
-          <input type="text" required value={name} />
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
