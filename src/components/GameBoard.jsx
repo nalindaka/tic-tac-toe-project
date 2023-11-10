46484ab5572e6d978057ee9b1665f101ba69c2be
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-11-11 00:15:31 +1000

    Disable clicked buttons

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -14,19 +14,6 @@ export default function GameBoard({ onSelectSquare, turns }) {
 
     gameBoard[row][col] = player;
   }
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectButton(rowIndex, colIndex) {
-  //   setGameBoard((previousGameBoard) => {
-  //     const updatedBoard = [
-  //       ...previousGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-
-  //   onSelectSquare();
-  // }
 
   return (
     <ol id="game-board">
@@ -35,7 +22,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
